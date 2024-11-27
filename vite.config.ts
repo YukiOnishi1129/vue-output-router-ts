5c45bdbbbabb141530eecccf3d851efe9b74d804
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-11-27 15:21:59 +0900

    setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vitest/config";
+import vue from "@vitejs/plugin-vue";
+import { REPOSITORY_NAME } from "./src/config/config";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  base: process.env.NODE_ENV === "production" ? `/${REPOSITORY_NAME}/` : "/",
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+});
